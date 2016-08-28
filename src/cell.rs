002e87d7c20e08ef32656bc0296168b299fb5860
Ignacio Corderi <icorderi@msn.com>
2016-08-28 15:56:20 -0700

    feat: add MutCell

--- src/cell.rs
+++ src/cell.rs
@@ -1,9 +1,9 @@
 pub struct PmemCell<T> {
-    ptr: *mut T,
+    ptr: *const T,
 }
 
 impl<T> PmemCell<T> {
-    pub unsafe fn new(ptr: *mut T) -> Self {
+    pub unsafe fn new(ptr: *const T) -> Self {
         PmemCell { ptr: ptr }
     }
 }
@@ -13,10 +13,6 @@ impl<T> ::std::ops::Deref for PmemCell<T> {
     fn deref(&self) -> &T { unsafe { &*self.ptr } }
 }
 
-impl<T> ::std::ops::DerefMut for PmemCell<T> {
-    fn deref_mut(&mut self) -> &mut T { unsafe { &mut *self.ptr } }
-}
-
 impl<T> ::std::fmt::Debug for PmemCell<T> where T: ::std::fmt::Debug {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
         write!(f, "{:?}", (self as &T))
@@ -24,6 +20,37 @@ impl<T> ::std::fmt::Debug for PmemCell<T> where T: ::std::fmt::Debug {
 }
 
 impl<T> ::std::fmt::Display for PmemCell<T> where T: ::std::fmt::Display {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "{}", (self as &T))
+    }
+}
+
+pub struct PmemMutCell<T> {
+    ptr: *mut T,
+}
+
+impl<T> PmemMutCell<T> {
+    pub unsafe fn new(ptr: *mut T) -> Self {
+        PmemMutCell { ptr: ptr }
+    }
+}
+
+impl<T> ::std::ops::Deref for PmemMutCell<T> {
+    type Target = T;
+    fn deref(&self) -> &T { unsafe { &*self.ptr } }
+}
+
+impl<T> ::std::ops::DerefMut for PmemMutCell<T> {
+    fn deref_mut(&mut self) -> &mut T { unsafe { &mut *self.ptr } }
+}
+
+impl<T> ::std::fmt::Debug for PmemMutCell<T> where T: ::std::fmt::Debug {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "{:?}", (self as &T))
+    }
+}
+
+impl<T> ::std::fmt::Display for PmemMutCell<T> where T: ::std::fmt::Display {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
         write!(f, "{}", (self as &T))
     }
